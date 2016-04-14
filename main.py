aea3c8f673fb59c3e9098637f86b77e4fa5c0d48
diyike <lzytianda@163.com>
2016-04-14 16:38:43 +0800

    dd

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
-#f
-#fgfdgd
-#changhong
-#houwe
-#gdgd
-66
+
 from ieee import get_abstract_ieee
 from springer import get_abstract_springer
 from acm import get_abstract_acm
